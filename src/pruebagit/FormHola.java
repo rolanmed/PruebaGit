7e3585fb1f427feedd1c0e93d4ceea0f918af3e7
rolanmed <rolanmed@gmail.com>
2017-10-19 10:28:21 -0600

    cambio de mensaje hola mundo

--- src/pruebagit/FormHola.java
+++ src/pruebagit/FormHola.java
@@ -59,7 +59,7 @@ public class FormHola extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        System.out.println("hola mundo");
+        System.out.println("hola mundo fgsdfgfgrd");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
