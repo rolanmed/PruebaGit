d76438e54012facae0ffe63cdf79a9024c9166a6
rolanmed <rolanmed@gmail.com>
2017-10-19 10:31:01 -0600

    cambio desde la nube
    dskfjasdkfjkla

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -15,7 +15,7 @@ public class PruebaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola desde la nube");
     }
     
 }
